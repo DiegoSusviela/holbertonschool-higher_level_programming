51d6da013b447da521748f9fdb2cd0a5a456cad5
DiegoSusviela <dieguitosus@hotmail.com>
2021-05-04 14:50:51 -0300

    added shebang in 4 and missspellded str in 5

--- 0x03-python-data_structures/4-new_in_list.py
+++ 0x03-python-data_structures/4-new_in_list.py
@@ -1,3 +1,4 @@
+#!/usr/bin/python3
 def new_in_list(my_list, idx, element):
     lis = my_list[:]
     if 0 <= idx < len(my_list):
--- 0x03-python-data_structures/5-no_c.py
+++ 0x03-python-data_structures/5-no_c.py
@@ -4,4 +4,4 @@ def no_c(my_string):
     for c in my_string:
         if c != 'c' and c != 'C':
             str1 += c
-    return sttr
+    return str1
