3899e3326fcd55dbd599757a890e51eb67a53027
DiegoSusviela <dieguitosus@hotmail.com>
2021-05-06 14:30:23 -0300

    fixed indentation 12

--- 0x04-python-more_data_structures/12-roman_to_int.py
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -2,7 +2,7 @@
 def roman_to_int(roman_string):
     if type(roman_string) == str:
         numero_romano = {'I': 1, 'V': 5, 'X': 10, 'L': 50, 'C': 100, 'D': 500,
-                     'M': 1000}
+                        'M': 1000}
         suma = 0
         encontro = 0
         for i in range(len(roman_string)):
