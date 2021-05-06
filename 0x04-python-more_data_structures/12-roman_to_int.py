de3dffe50eae8b52b2559af93b8bb6f5bb2de475
DiegoSusviela <dieguitosus@hotmail.com>
2021-05-06 14:28:11 -0300

    added 12

---
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -0,0 +1,37 @@
+#!/usr/bin/python3
+def roman_to_int(roman_string):
+    if type(roman_string) == str:
+        numero_romano = {'I': 1, 'V': 5, 'X': 10, 'L': 50, 'C': 100, 'D': 500,
+                     'M': 1000}
+        suma = 0
+        encontro = 0
+        for i in range(len(roman_string)):
+            if encontro:
+                encontro = 0
+                continue
+            for key in numero_romano:
+                if roman_string[i] == key and i != len(roman_string) - 1:
+                    if roman_string[i] == 'I' and roman_string[i + 1] == 'V':
+                        suma += 4
+                        encontro = 1
+                    elif roman_string[i] == 'I' and roman_string[i + 1] == 'X':
+                        suma += 9
+                        encontro = 1
+                    elif roman_string[i] == 'X' and roman_string[i + 1] == 'L':
+                        suma += 40
+                        encontro = 1
+                    elif roman_string[i] == 'X' and roman_string[i + 1] == 'C':
+                        suma += 90
+                        encontro = 1
+                    elif roman_string[i] == 'C' and roman_string[i + 1] == 'D':
+                        suma += 400
+                        encontro = 1
+                    elif roman_string[i] == 'C' and roman_string[i + 1] == 'M':
+                        suma += 900
+                        encontro = 1
+                    else:
+                        suma += numero_romano[key]
+                elif roman_string[i] == key:
+                    suma += numero_romano[key]
+        return suma
+    return 0
