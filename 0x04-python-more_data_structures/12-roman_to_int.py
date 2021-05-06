97af500db51e92a3266485284eab9834070cd666
DiegoSusviela <dieguitosus@hotmail.com>
2021-05-06 14:30:50 -0300

    removed new line in indentation

--- 0x04-python-more_data_structures/12-roman_to_int.py
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -1,8 +1,7 @@
 #!/usr/bin/python3
 def roman_to_int(roman_string):
     if type(roman_string) == str:
-        numero_romano = {'I': 1, 'V': 5, 'X': 10, 'L': 50, 'C': 100, 'D': 500,
-                        'M': 1000}
+        numero_romano = {'I': 1, 'V': 5, 'X': 10, 'L': 50, 'C': 100, 'D': 500,'M': 1000}
         suma = 0
         encontro = 0
         for i in range(len(roman_string)):
