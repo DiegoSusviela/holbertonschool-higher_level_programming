b17ef56fdf1ade11ca4f48d3bc8b2869f682e4d1
DiegoSusviela <dieguitosus@hotmail.com>
2021-05-18 22:59:45 -0300

    chyange all 100 documentations

--- 0x06-python-classes/100-singly_linked_list.py
+++ 0x06-python-classes/100-singly_linked_list.py
@@ -2,43 +2,75 @@
 
 
 class Node:
-    """asd"""
+    """node in singly linked
+    Attributes:
+        __data (int): data
+        __next_node (Node): next node
+    """
     def __init__(self, data, next_node=None):
-        """asd"""
+        """Initializes
+        Args:
+            data (int): data
+            next_node (Node): next node
+        Returns:
+            None
+        """
         self.data = data
         self.next_node = next_node
 
     @property
     def data(self):
-        """asd"""
+        """data
+        Returns:
+            data
+        """
         return self.__data
 
     @data.setter
     def data(self, value):
-        """asd"""
+        """data
+        Args:
+            value (int): datastored
+        Returns:
+            None
+        """
         if type(value) is not int:
             raise TypeError("data must be an integer")
         self.__data = value
 
     @property
     def next_node(self):
-        """asd"""
+        """get next_node
+        Returns:
+           next node
+        """
         return self.__next_node
 
     @next_node.setter
     def next_node(self, value):
-        """asd"""
+        """__next_node
+        Args:
+            value (Node): next node
+        Returns:
+            None
+        """
         if value is not None and type(value) is not Node:
             raise TypeError("next_node must be a Node object")
         self.__next_node = value
 
     def __str__(self):
-        """asd"""
+        """String
+        Returns:
+            Formatted string 
+        """
         return str(self.__data)
 
 
 class SinglyLinkedList:
-    """asd"""
+    """Representer
+    Attributes:
+        __head (Node): head
+    """
     def __init__(self):
         """Initializer
         Returns:
