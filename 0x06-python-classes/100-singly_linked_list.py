d6793b6ffb2a006f99f6accecc61702680e90818
DiegoSusviela <dieguitosus@hotmail.com>
2021-05-18 21:26:14 -0300

    added first advanced

---
+++ 0x06-python-classes/100-singly_linked_list.py
@@ -0,0 +1,71 @@
+#!/usr/bin/python3
+
+
+class Node:
+    """asdasd"""
+    def __init__(self, data, next_node=None):
+        """asdasd"""
+        self.data = data
+        self.next_node = next_node
+
+    @property
+    def data(self):
+        """asdasd"""
+        return self.__data
+
+    @data.setter
+    def data(self, value):
+        """asdasd"""
+        if type(value) is not int:
+            raise TypeError("data must be an integer")
+        self.__data = value
+
+    @property
+    def next_node(self):
+        """asdasd"""
+        return self.__next_node
+
+    @next_node.setter
+    def next_node(self, value):
+        """asdasd"""
+        if value is not None and type(value) is not Node:
+            raise TypeError("next_node must be a Node object")
+        self.__next_node = value
+
+    def __str__(self):
+        """asdasd"""
+        return str(self.__data)
+
+
+class SinglyLinkedList:
+    """asdasd"""
+    def __init__(self):
+        """asdasd"""
+        self.__head = None
+
+    def sorted_insert(self, value):
+        """asdasd"""
+        new = Node(value)
+        tmp = self.__head
+        if tmp is None or tmp.data >= value:
+            if tmp:
+                new.next_node = tmp
+            self.__head = new
+            return
+        while tmp.next_node is not None:
+            if tmp.next_node.data >= value:
+                break
+            tmp = tmp.next_node
+        new.next_node = tmp.next_node
+        tmp.next_node = new
+
+    def __str__(self):
+        """asdasd"""
+        string = ""
+        tmp = self.__head
+        while tmp is not None:
+            string += str(tmp)
+            if tmp.next_node is not None:
+                string += "\n"
+            tmp = tmp.next_node
+        return string
