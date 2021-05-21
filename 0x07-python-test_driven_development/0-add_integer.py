e56ff3658645a397b01250a9d8aa919f5a249be1
DiegoSusviela <dieguitosus@hotmail.com>
2021-05-21 14:56:58 -0300

    tidying upo the code, should be now working

--- 0x07-python-test_driven_development/0-add_integer.py
+++ 0x07-python-test_driven_development/0-add_integer.py
@@ -6,14 +6,14 @@ This module supplies with one function, add_integer(a, b)
 """
 
 
-def add_integer(a, b=0):
+def add_integer(a, b=98):
     """Returns la suma vio"""
-    if type(a) is not int and type(a) is not float:
-        raise TypeError("a must be an integer")
-    if type(b) is not int and type(b) is not float:
-        raise TypeError("b must be an integer")
     if type(a) is float:
         a = int(a)
     if type(b) is float:
         b = int(b)
+    if type(a) is not int:
+        raise TypeError("a must be an integer")
+    if type(b) is not int:
+        raise TypeError("b must be an integer")
     return a + b
