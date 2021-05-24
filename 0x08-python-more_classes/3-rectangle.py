31edef78aeb9b98c24e472d36ca6ca9437080399
DiegoSusviela <dieguitosus@hotmail.com>
2021-05-24 18:38:55 -0300

    fixed 3 for all checks 5

--- 0x08-python-more_classes/3-rectangle.py
+++ 0x08-python-more_classes/3-rectangle.py
@@ -51,5 +51,6 @@ class Rectangle:
         """printable representation of omegalul"""
         string = ""
         if self.__width != 0 and self.__height != 0:
-            string += "\n".join("#" * self.__width for j in range(self.__height))
+            string += "\n".join("#" * self.__width
+                                for j in range(self.__height))
         return string
