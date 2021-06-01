6fcc167fb96b3c92ca493da41a6783cce8c9c27f
DiegoSusviela <dieguitosus@hotmail.com>
2021-06-01 18:40:33 -0300

    added 101

---
+++ 0x0A-python-inheritance/101-add_attribute.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+"""module"""
+
+
+def add_attribute(obj, objname, value):
+    """omegalul empty"""
+    if hasattr(obj, "__dict__") == False:
+        raise TypeError("can't add new attribute")
+    setattr(obj, objname, value)
