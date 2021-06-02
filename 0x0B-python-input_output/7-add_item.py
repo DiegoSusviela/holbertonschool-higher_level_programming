bbff4fde9516baa2ff7b80bcecce6459f7998fb9
DiegoSusviela <dieguitosus@hotmail.com>
2021-06-02 18:51:53 -0300

    changed 7 without poath

--- 0x0B-python-input_output/7-add_item.py
+++ 0x0B-python-input_output/7-add_item.py
@@ -1,14 +1,18 @@
 #!/usr/bin/python3
 """script adds args to list"""
 
-from os import path
 from sys import argv
-save_to_json_file = __import__('5-save_to_json_file.py').save_to_json_file
-load_from_json_file = __import__('6-load_from_json_file.py').load_from_json_file
+save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
+load_from_json_file = __import__('6-load_from_json_file').load_from_json_file
 
-if path.exists("add_item.json") is False:
-    save_to_json_file([], "add_item.json")
-filename = load_from_json_file("add_item.json")
-for i in range(1, len(argv)):
-    filename.append(argv[i])
-save_to_json_file(filename, "add_item.json")
+filename = "add_item.json"
+
+try:
+    json_list = load_from_json_file(filename)
+except:
+    json_list = []
+
+for arg in argv[1:]:
+    json_list.append(arg)
+
+save_to_json_file(json_list, filename)
