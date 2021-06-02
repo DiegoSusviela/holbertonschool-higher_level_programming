3f84df01255a27b5afca5221dd398562b550cb18
DiegoSusviela <dieguitosus@hotmail.com>
2021-06-02 18:59:08 -0300

    added stundents

---
+++ 0x0B-python-input_output/10-student.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+"""script adds args to list"""
+
+
+class Student:
+    def __init__(self, first_name, last_name, age):
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+
+    def to_json(self, attrs=None):
+        if type(attrs) is list and all(type(x) is str for x in attrs):
+            dictionary = {}
+            for i in attrs:
+                if i in self.__dict__:
+                    dictionary[i] = self.__dict__[i]
+            return dictionary
+        else:
+            return self.__dict__
---
+++ 0x0B-python-input_output/11-student.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python3
+"""script adds args to list"""
+
+
+class Student:
+    def __init__(self, first_name, last_name, age):
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+
+    def to_json(self, attrs=None):
+        if type(attrs) is list and all(type(x) is str for x in attrs):
+            dictionary = {}
+            for i in attrs:
+                if i in self.__dict__:
+                    dictionary[i] = self.__dict__[i]
+            return dictionary
+        else:
+            return self.__dict__
+
+    def reload_from_json(self, json):
+        for key, value in json.items():
+            self.__dict__[key] = value
