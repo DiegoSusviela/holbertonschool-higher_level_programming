77766a7b0ba95e9cafc8e6422cec7a6b9d990d06
DiegoSusviela <dieguitosus@hotmail.com>
2021-06-02 18:24:54 -0300

    wrong number excercice

---
+++ 0x0B-python-input_output/2-append_write.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+""""append_write" function"""
+
+
+def append_write(filename="", text=""):
+    """omegalul empty"""
+    with open(filename, 'a', encoding='utf-8') as file:
+        return file.write(text)
