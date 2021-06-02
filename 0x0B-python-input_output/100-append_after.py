29a5ee5518f3166b74d286c37c32ddd71deae7ed
DiegoSusviela <dieguitosus@hotmail.com>
2021-06-02 19:10:09 -0300

    added first advanced

---
+++ 0x0B-python-input_output/100-append_after.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+"""read_file function"""
+
+
+def append_after(filename="", search_string="", new_string=""):
+    with open(filename, 'r', encoding='utf-8') as f:
+        lista = []
+        while True:
+            linea = f.readline()
+            if linea == "":
+                break
+            lista.append(linea)
+            if search_string in linea:
+                lista.append(new_string)
+    with open(filename, 'w', encoding='utf-8') as f:
+        f.writelines(lista)
