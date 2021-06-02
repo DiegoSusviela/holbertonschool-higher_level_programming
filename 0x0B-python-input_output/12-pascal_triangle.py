2b0602204ebefad982713a3bccc0a8de25b5981e
DiegoSusviela <dieguitosus@hotmail.com>
2021-06-02 19:11:31 -0300

    changed name to pascal from triangle, smh

--- 0x0B-python-input_output/12-pascal_triangle.py
+++ 0x0B-python-input_output/12-pascal_triangle.py
@@ -2,12 +2,12 @@
 def pascal_triangle(n):
     if n <= 0:
         return []
-    triangle = []
+    pascal = []
     row = []
     for i in range(n):
         row = [1]
         if i > 0:
             for j in range(i):
-                row.append(sum(triangle[-1][j:j+2]))
-        triangle.append(row)
-    return triangle
+                row.append(sum(pascal[-1][j:j+2]))
+        pascal.append(row)
+    return pascal
