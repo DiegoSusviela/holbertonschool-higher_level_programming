c706f9581c12e183181c6576b95bfe4da98de716
DiegoSusviela <dieguitosus@hotmail.com>
2021-06-02 18:34:38 -0300

    i was on the wrong fucking file, amazing

--- 0x0B-python-input_output/1-write_file.py
+++ 0x0B-python-input_output/1-write_file.py
@@ -2,7 +2,7 @@
 """append_write function"""
 
 
-def append_write(filename="", text=""):
+def write_file(filename="", text=""):
     """omegalul empty"""
-    with open(filename, 'a', encoding='utf-8') as file:
-        return file.write(text)
+    with open(filename, 'w', encoding='utf=8') as f:
+        return f.write(text)
