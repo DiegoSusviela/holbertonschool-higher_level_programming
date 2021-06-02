dae03916a4e1c5740b0cc023a363bb889108371d
DiegoSusviela <dieguitosus@hotmail.com>
2021-06-02 18:47:23 -0300

    typos

--- 0x0B-python-input_output/7-add_item.py
+++ 0x0B-python-input_output/7-add_item.py
@@ -3,8 +3,8 @@
 
 from os import path
 from sys import argv
-save_to_json_file = __import__('7-save_to_json_file').save_to_json_file
-load_from_json_file = __import__('8-load_from_json_file').load_from_json_file
+save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
+load_from_json_file = __import__('6-load_from_json_file').load_from_json_file
 
 if path.exists("add_item.json") is False:
     save_to_json_file([], "add_item.json")
