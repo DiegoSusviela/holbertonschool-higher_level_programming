13881fee7e91ffdfa86928c4dfa0351caa3dd3e7
DiegoSusviela <dieguitosus@hotmail.com>
2021-06-08 19:15:03 -0300

    idk, im not testing the rest of things tbh

---
+++ 0x0C-python-almost_a_circle/tests/test_models/test_base.py
@@ -0,0 +1,118 @@
+#!/usr/bin/python3
+"""a cagar"""
+
+import unittest
+import inspect
+import pep8
+import json
+from models import base
+Base = base.Base
+
+
+class TestBaseDocs(unittest.TestCase):
+    """a cagar"""
+    @classmethod
+    def setUpClass(cls):
+        """a cagar"""
+        cls.base_funcs = inspect.getmembers(Base, inspect.isfunction)
+
+    def test_pep8_conformance_base(self):
+        """a cagar"""
+        pep8style = pep8.StyleGuide(quiet=True)
+        result = pep8style.check_files(['models/base.py'])
+        self.assertEqual(result.total_errors, 0,
+                         "Found code style errors (and warnings).")
+
+    def test_pep8_conformance_test_base(self):
+        """a cagar"""
+        pep8style = pep8.StyleGuide(quiet=True)
+        result = pep8style.check_files(['tests/test_models/test_base.py'])
+        self.assertEqual(result.total_errors, 0,
+                         "Found code style errors (and warnings).")
+
+    def test_module_docstring(self):
+        """a cagar"""
+        self.assertTrue(len(base.__doc__) >= 1)
+
+    def test_class_docstring(self):
+        """a cagar"""
+        self.assertTrue(len(Base.__doc__) >= 1)
+
+    def test_func_docstrings(self):
+        """a cagar"""
+        for func in self.base_funcs:
+            self.assertTrue(len(func[1].__doc__) >= 1)
+
+
+class TestBase(unittest.TestCase):
+    """a cagar"""
+    def test_too_many_args(self):
+        """a cagar"""
+        with self.assertRaises(TypeError):
+            b = Base(1, 1)
+
+    def test_no_id(self):
+        """a cagar"""
+        b = Base()
+        self.assertEqual(b.id, 1)
+
+    def test_id_set(self):
+        """a cagar"""
+        b98 = Base(98)
+        self.assertEqual(b98.id, 98)
+
+    def test_no_id_after_set(self):
+        """a cagar"""
+        b2 = Base()
+        self.assertEqual(b2.id, 2)
+
+    def test_nb_private(self):
+        """a cagar"""
+        b = Base(3)
+        with self.assertRaises(AttributeError):
+            print(b.nb_objects)
+        with self.assertRaises(AttributeError):
+            print(b.__nb_objects)
+
+    def test_to_json_string(self):
+        """a cagar"""
+        Base._Base__nb_objects = 0
+        d1 = {"id": 9, "width": 5, "height": 6, "x": 7, "y": 8}
+        d2 = {"id": 2, "width": 2, "height": 3, "x": 4, "y": 0}
+        json_s = Base.to_json_string([d1, d2])
+        self.assertTrue(type(json_s) is str)
+        d = json.loads(json_s)
+        self.assertEqual(d, [d1, d2])
+
+    def test_empty_to_json_string(self):
+        """a cagar"""
+        json_s = Base.to_json_string([])
+        self.assertTrue(type(json_s) is str)
+        self.assertEqual(json_s, "[]")
+
+    def test_None_to_json_String(self):
+        json_s = Base.to_json_string(None)
+        self.assertTrue(type(json_s) is str)
+        self.assertEqual(json_s, "[]")
+
+    def test_from_json_string(self):
+        """a cagar"""
+        json_str = '[{"id": 9, "width": 5, "height": 6, "x": 7, "y": 8}, \
+{"id": 2, "width": 2, "height": 3, "x": 4, "y": 0}]'
+        json_l = Base.from_json_string(json_str)
+        self.assertTrue(type(json_l) is list)
+        self.assertEqual(len(json_l), 2)
+        self.assertTrue(type(json_l[0]) is dict)
+        self.assertTrue(type(json_l[1]) is dict)
+        self.assertEqual(json_l[0],
+                         {"id": 9, "width": 5, "height": 6, "x": 7, "y": 8})
+        self.assertEqual(json_l[1],
+                         {"id": 2, "width": 2, "height": 3, "x": 4, "y": 0})
+
+    def test_fjs_empty(self):
+        """a cagar"""
+        self.assertEqual([], Base.from_json_string(""))
+
+    def test_fjs_None(self):
+        """a cagar"""
+        self.assertEqual([], Base.from_json_string(None))
---
+++ 0x0C-python-almost_a_circle/tests/test_models/test_square.py
@@ -0,0 +1,281 @@
+#!/usr/bin/python3
+"""a cagar"""
+
+
+import unittest
+import io
+import contextlib
+from models.square import Square
+from models.base import Base
+from models.rectangle import Rectangle
+
+
+class TestBaseClass(unittest.TestCase):
+    """a cagar"""
+    def test_SquareinheritancefromBase(self):
+        """a cagar"""
+        Square.reset_objects()
+        self.assertEqual(issubclass(Square, Base), True)
+
+    def test_SquareinheritancefromRectangle(self):
+        """a cagar"""
+        Square.reset_objects()
+        self.assertEqual(issubclass(Square, Rectangle), True)
+
+    def test_objectinheritance(self):
+        """a cagar"""
+        Square.reset_objects()
+        s1 = Square(5)
+        self.assertEqual(isinstance(s1, Square), True)
+
+    def test_errorfornoarguments(self):
+        """a cagar"""
+        Square.reset_objects()
+        with self.assertRaises(TypeError) as e:
+            s1 = Square()
+        self.assertEqual(
+            str(e.exception),
+            "__init__() missing 1 required positional argument: 'size'")
+
+    def test_errorfortoomanyarguments(self):
+        """a cagar"""
+        Square.reset_objects()
+        with self.assertRaises(TypeError) as e:
+            s1 = Square(1, 2, 3, 4, 5)
+        self.assertEqual(
+            str(e.exception),
+            "__init__() takes from 2 to 5 positional" +
+            " arguments but 6 were given")
+
+    def test_negativesize(self):
+        """a cagar"""
+        Square.reset_objects()
+        with self.assertRaises(ValueError) as e:
+            s1 = Square(-1)
+        self.assertEqual(str(e.exception), "width must be > 0")
+
+    def test_negativexvalue(self):
+        """a cagar"""
+        Square.reset_objects()
+        with self.assertRaises(ValueError) as e:
+            s1 = Square(1, -2)
+        self.assertEqual(str(e.exception), "x must be >= 0")
+
+    def test_negativeyvalue(self):
+        """a cagar"""
+        Square.reset_objects()
+        with self.assertRaises(ValueError) as e:
+            s1 = Square(1, 2, -2)
+        self.assertEqual(str(e.exception), "y must be >= 0")
+
+    def test_0size(self):
+        """a cagar"""
+        Square.reset_objects()
+        with self.assertRaises(ValueError) as e:
+            s1 = Square(0)
+        self.assertEqual(str(e.exception), "width must be > 0")
+
+    def test_singlesquarecreation(self):
+        """a cagar"""
+        Square.reset_objects()
+        s1 = Square(10)
+        self.assertEqual(s1.id, 1)
+
+    def test_multiplesquarecreation(self):
+        """a cagar"""
+        Square.reset_objects()
+        s1 = Square(10)
+        s2 = Square(2)
+        s3 = Square(3)
+        self.assertEqual(s1.id, 1)
+        self.assertEqual(s2.id, 2)
+        self.assertEqual(s3.id, 3)
+
+    def test_singlesquarecreationwithallvalues(self):
+        """a cagar"""
+        Square.reset_objects()
+        s1 = Square(10, 10, 10, 10)
+        self.assertEqual(s1.width, 10)
+        self.assertEqual(s1.height, 10)
+        self.assertEqual(s1.x, 10)
+        self.assertEqual(s1.y, 10)
+        self.assertEqual(s1.id, 10)
+        s2 = Square(10, 10, 10, 10)
+        self.assertEqual(s2.width, 10)
+        self.assertEqual(s2.height, 10)
+        self.assertEqual(s2.x, 10)
+        self.assertEqual(s2.y, 10)
+        self.assertEqual(s2.id, 10)
+
+    def test_multiplesquarecreationwithallvalues(self):
+        """a cagar"""
+        Square.reset_objects()
+        s1 = Square(10, 10, 10, 10)
+        self.assertEqual(s1.width, 10)
+        self.assertEqual(s1.height, 10)
+        self.assertEqual(s1.x, 10)
+        self.assertEqual(s1.y, 10)
+        self.assertEqual(s1.id, 10)
+
+    def test_badsizevaluewithstring(self):
+        """a cagar"""
+        Rectangle.reset_objects()
+        with self.assertRaises(TypeError) as e:
+            r1 = Square("foo", 1, 2, 3)
+        self.assertEqual(str(e.exception), 'width must be an integer')
+
+    def test_badxvaluewithstring(self):
+        """a cagar"""
+        Rectangle.reset_objects()
+        with self.assertRaises(TypeError) as e:
+            r1 = Square(1, "foo", 2, 3)
+        self.assertEqual(str(e.exception), 'x must be an integer')
+
+    def test_badyvaluewithstring(self):
+        """a cagar"""
+        Rectangle.reset_objects()
+        with self.assertRaises(TypeError) as e:
+            r1 = Square(1, 2, "foo", 3)
+        self.assertEqual(str(e.exception), 'y must be an integer')
+
+    def test_badyvaluewithsets(self):
+        """a cagar"""
+        Rectangle.reset_objects()
+        with self.assertRaises(TypeError) as e:
+            r1 = Square(1, 2, {1, 2, 3}, 3)
+        self.assertEqual(str(e.exception), 'y must be an integer')
+
+    def test_badsizevaluedicts(self):
+        """a cagar"""
+        Rectangle.reset_objects()
+        with self.assertRaises(TypeError) as e:
+            r1 = Square({"foo": 1}, 1, 2, 3)
+        self.assertEqual(str(e.exception), 'width must be an integer')
+
+    def test_badxvaluewithdicts(self):
+        """a cagar"""
+        Rectangle.reset_objects()
+        with self.assertRaises(TypeError) as e:
+            r1 = Square(1, {"foo": 1}, 2, 3)
+        self.assertEqual(str(e.exception), 'x must be an integer')
+
+    def test_badyvaluewithdicts(self):
+        """a cagar"""
+        Rectangle.reset_objects()
+        with self.assertRaises(TypeError) as e:
+            r1 = Square(1, 2, {"foo": 1}, 3)
+        self.assertEqual(str(e.exception), 'y must be an integer')
+
+    def test_badsizevaluefuncs(self):
+        """a cagar"""
+        Rectangle.reset_objects()
+        with self.assertRaises(TypeError) as e:
+            r1 = Square(print(), 1, 2, 3)
+        self.assertEqual(str(e.exception), 'width must be an integer')
+
+    def test_badxvaluewithfuncs(self):
+        """a cagar"""
+        Rectangle.reset_objects()
+        with self.assertRaises(TypeError) as e:
+            r1 = Square(1, print(), 2, 3)
+        self.assertEqual(str(e.exception), 'x must be an integer')
+
+    def test_badyvaluewithfuncs(self):
+        """a cagar"""
+        Rectangle.reset_objects()
+        with self.assertRaises(TypeError) as e:
+            r1 = Square(1, 2, print(), 3)
+        self.assertEqual(str(e.exception), 'y must be an integer')
+
+    def test_NaNsize(self):
+        """a cagar"""
+        Rectangle.reset_objects()
+        with self.assertRaises(TypeError) as e:
+            r1 = Square(float('nan'), 10, 5, 7)
+        self.assertEqual(str(e.exception), 'width must be an integer')
+
+    def test_NaNx(self):
+        """a cagar"""
+        Rectangle.reset_objects()
+        with self.assertRaises(TypeError) as e:
+            r1 = Square(10, float('nan'), 5, 7)
+        self.assertEqual(str(e.exception), 'x must be an integer')
+
+    def test_NaNy(self):
+        """a cagar"""
+        Rectangle.reset_objects()
+        with self.assertRaises(TypeError) as e:
+            r1 = Square(10, 10, float('nan'), 7)
+        self.assertEqual(str(e.exception), 'y must be an integer')
+
+    def test_sizesetterwithfunc(self):
+        """a cagar"""
+        Rectangle.reset_objects()
+        with self.assertRaises(TypeError) as e:
+            r1 = Square(1, 2, 2, 3)
+            self.assertEqual(r1.size, 1)
+            r1.size = print()
+        self.assertEqual(str(e.exception), "width must be an integer")
+
+    def test_kwargsskipped(self):
+        """a cagar"""
+        Rectangle.reset_objects()
+        s1 = Square(10, 10, 10, 10)
+        s1.update(1, 2, 3, 4, id=10)
+        self.assertEqual(s1.id, 1)
+
+    def test_updatebadsizevalue(self):
+        """a cagar"""
+        Square.reset_objects()
+        r1 = Square(1, 2, 3, 4)
+        r1.update(1, "foo")
+        self.assertEqual(r1.size, 1)
+
+    def test_updatebadxvalue(self):
+        """a cagar"""
+        Square.reset_objects()
+        r1 = Square(1, 2, 3, 4)
+        r1.update(1, 2, "foo")
+        self.assertEqual(r1.x, 2)
+
+    def test_updatebadyvalue(self):
+        """a cagar"""
+        Square.reset_objects()
+        r1 = Square(1, 2, 3, 4)
+        r1.update(1, 2, 3, "foo")
+        self.assertEqual(r1.y, 3)
+
+    def test_updateallbadvalues(self):
+        """a cagar"""
+        Square.reset_objects()
+        r1 = Square(1, 2, 3, 4)
+        r1.update([1], (9, ), True, "foo")
+        self.assertEqual(r1.size, 1)
+        self.assertEqual(r1.x, 2)
+        self.assertEqual(r1.y, 3)
+        self.assertEqual(r1.id, 4)
+
+    def test_to_dict(self):
+        """a cagar"""
+        Square.reset_objects()
+        s1 = Square(10, 2, 1)
+        s1_dictionary = s1.to_dictionary()
+        self.assertEqual(s1_dictionary, {'id': 1, 'x': 2, 'size': 10, 'y': 1})
+
+    def test_display__method(self):
+        """a cagar"""
+        Rectangle.reset_objects()
+        s1 = Square(5)
+        f = io.StringIO()
+        with contextlib.redirect_stdout(f):
+            s1.display()
+        self.assertEqual(f.getvalue(), "#####\n#####\n#####\n#####\n#####\n")
+
+    def test_display__method2(self):
+        """a cagar"""
+        Rectangle.reset_objects()
+        s2 = Square(2, 2)
+        f = io.StringIO()
+        with contextlib.redirect_stdout(f):
+            s2.display()
+        self.assertEqual(f.getvalue(), "  ##\n  ##\n")
