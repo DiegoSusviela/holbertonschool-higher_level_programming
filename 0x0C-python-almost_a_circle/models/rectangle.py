5cf8a2ad945ec9acf5b4ddfc3facc69be4574212
DiegoSusviela <dieguitosus@hotmail.com>
2021-06-08 18:38:12 -0300

    missing a space

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -71,10 +71,10 @@ class Rectangle(Base):
 
     def __str__(self):
         return "[Rectangle] ({:d}) {:d}/{:d} - {:d}/{:d}".format(self.id,
-                                                                self.__x,
-                                                                self.__y,
-                                                                self.__width,
-                                                                self.__height)
+                                                                 self.__x,
+                                                                 self.__y,
+                                                                 self.__width,
+                                                                 self.__height)
 
     def update(self, *args, **kwargs):
         if len(args):
