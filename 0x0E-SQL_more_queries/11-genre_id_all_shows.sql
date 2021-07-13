faec6feeaca5186bc28bd3fe85f4cb21b0f9666a
DiegoSusviela <dieguitosus@hotmail.com>
2021-07-13 19:20:02 -0300

    added everything before starting advanced, fix als

--- 0x0E-SQL_more_queries/11-genre_id_all_shows.sql
+++ 0x0E-SQL_more_queries/11-genre_id_all_shows.sql
@@ -2,5 +2,5 @@
 -- because Batch 3 is the best!
 SELECT tv_shows.title, tv_show_genres.genre_id
 FROM tv_shows
-RIGHT JOIN tv_show_genres ON tv_show_genres.show_id = tv_shows.id
+LEFT JOIN tv_show_genres ON tv_show_genres.show_id = tv_shows.id
 ORDER BY tv_shows.title, tv_show_genres.genre_id;
--- 0x0E-SQL_more_queries/12-no_genre.sql
+++ 0x0E-SQL_more_queries/12-no_genre.sql
@@ -2,5 +2,7 @@
 -- because Batch 3 is the best!
 SELECT tv_shows.title, tv_show_genres.genre_id
 FROM tv_shows
-RIGHT JOIN tv_show_genres ON tv_show_genres.show_id = tv_shows.id
+LEFT JOIN tv_show_genres
+ON tv_shows.id=tv_show_genres.show_id
+WHERE tv_show_genres.show_id IS NULL
 ORDER BY tv_shows.title, tv_show_genres.genre_id;
