b53f2b66e972d716c5e246ed9401cf7dd19852cb
DiegoSusviela <dieguitosus@hotmail.com>
2021-07-12 23:56:05 -0300

    fixing advacned

--- 0x0D-SQL_introduction/101-avg_temperatures.sql
+++ 0x0D-SQL_introduction/101-avg_temperatures.sql
@@ -1,3 +1,8 @@
 -- 3 first students in the Batch ID=3
 -- because Batch 3 is the best!
-SELECT city, AVG(value) AS avg_temp FROM temperatures GROUP BY city ORDER BY avg_temp DESC;
+SELECT city, AVG(value) as avg_temp
+FROM temperatures
+WHERE month BETWEEN 7 AND 8
+GROUP BY city
+ORDER BY avg_temp DESC
+LIMIT 3;
--- 0x0D-SQL_introduction/103-max_state.sql
+++ 0x0D-SQL_introduction/103-max_state.sql
@@ -1,3 +1,6 @@
 -- 3 first students in the Batch ID=3
 -- because Batch 3 is the best!
-SELECT state, MAX(value) as max_temp FROM temperatures GROUP BY state ORDER BY state;
+SELECT state, MAX(value) as max_temp
+FROM temperatures
+GROUP BY state
+ORDER BY state ASC;
