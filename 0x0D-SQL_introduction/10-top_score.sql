46e97f0e064a424669355759b6ab61eaa898750d
DiegoSusviela <dieguitosus@hotmail.com>
2021-07-12 23:47:48 -0300

    commiting everything, pre resolution

--- 0x0D-SQL_introduction/10-top_score.sql
+++ 0x0D-SQL_introduction/10-top_score.sql
@@ -1,3 +1,3 @@
 -- 3 first students in the Batch ID=3
 -- because Batch 3 is the best!
-CREATE TABLE IF NOT EXISTS first_table (id INT, name VARCHAR(256));
+SELECT score, name FROM second_table ORDER BY score DESC;
--- 0x0D-SQL_introduction/14-average.sql
+++ 0x0D-SQL_introduction/14-average.sql
@@ -1,3 +1,3 @@
 -- 3 first students in the Batch ID=3
 -- because Batch 3 is the best!
-CREATE TABLE IF NOT EXISTS first_table (id INT, name VARCHAR(256));
+SELECT AVG(score) AS average FROM second_table;
