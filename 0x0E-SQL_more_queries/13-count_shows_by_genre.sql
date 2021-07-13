53f16131aa8c5ea54e751f28248f6fade60d3ef6
DiegoSusviela <dieguitosus@hotmail.com>
2021-07-13 19:21:38 -0300

    changed * in count(), to .id

--- 0x0E-SQL_more_queries/13-count_shows_by_genre.sql
+++ 0x0E-SQL_more_queries/13-count_shows_by_genre.sql
@@ -1,6 +1,6 @@
 -- 3 first students in the Batch ID=3
 -- because Batch 3 is the best!
-SELECT tv_genres.name AS genre, COUNT(*) AS number_shows
+SELECT tv_genres.name AS genre, COUNT(tv_show_genres.show_id) AS number_shows
 FROM tv_genres
 INNER JOIN tv_show_genres
 ON tv_genres.id = tv_show_genres.genre_id
