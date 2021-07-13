137a747c5c7fcd6ab4191cdc9265229124b750d2
DiegoSusviela <dieguitosus@hotmail.com>
2021-07-13 20:23:28 -0300

    added last advacned

--- 0x0E-SQL_more_queries/103-rating_genres.sql
+++ 0x0E-SQL_more_queries/103-rating_genres.sql
@@ -1,3 +1,7 @@
 -- 3 first students in the Batch ID=3
 -- because Batch 3 is the best!
-
+SELECT tv_genres.name, SUM(tv_show_ratings.rate) AS rating FROM tv_genres
+INNER JOIN tv_show_genres ON tv_genres.id = tv_show_genres.genre_id
+INNER JOIN tv_show_ratings ON tv_show_genres.show_id = tv_show_ratings.show_id
+GROUP BY tv_genres.id
+ORDER BY rating DESC;
