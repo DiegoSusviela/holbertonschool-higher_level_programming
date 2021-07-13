755e7f05ad14e16ddd5023f7378dd957a2d32e9b
DiegoSusviela <dieguitosus@hotmail.com>
2021-07-13 20:55:23 -0300

    fiuxed 102

--- 0x0E-SQL_more_queries/102-rating_shows.sql
+++ 0x0E-SQL_more_queries/102-rating_shows.sql
@@ -1,6 +1,8 @@
 -- 3 first students in the Batch ID=3
 -- because Batch 3 is the best!
-SELECT tv_shows.title, SUM(tv_show_ratings.rate) AS rating FROM tv_shows
-INNER JOIN tv_show_ratings ON tv_shows.id = tv_show_ratings.show_id
+SELECT tv_shows.title, SUM(tv_show_ratings.rate) AS rating
+FROM tv_shows
+INNER JOIN tv_show_ratings
+ON tv_shows.id = tv_show_ratings.show_id
 GROUP BY tv_shows.id
 ORDER BY rating DESC;
