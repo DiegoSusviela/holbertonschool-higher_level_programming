db361fd1bf4d85afa2c025735292cd412f21e35a
DiegoSusviela <dieguitosus@hotmail.com>
2021-08-02 19:54:53 -0300

    fixzing () in 10

--- 0x12-javascript-warm_up/10-factorial.js
+++ 0x12-javascript-warm_up/10-factorial.js
@@ -4,6 +4,6 @@ function factorial (x) {
   if (isNaN(number) || x === 1) {
     return 1;
   }
-  return x * factorial(number - 1);
+  return (x * factorial(number - 1));
 }
 console.log(factorial(process.argv[2]));
