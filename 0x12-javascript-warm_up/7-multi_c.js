4c5d88757c936be4deaed60de9e5644d7bf4aa27
DiegoSusviela <dieguitosus@hotmail.com>
2021-08-02 20:02:44 -0300

    same as be

--- 0x12-javascript-warm_up/7-multi_c.js
+++ 0x12-javascript-warm_up/7-multi_c.js
@@ -2,7 +2,7 @@
 if (isNaN(process.argv[2])) {
   console.log('Missing number of occurrences');
 } else {
-  for (let iter = 0; i < process.argv[2]; iter++) {
+  for (let iter = 0; iter < process.argv[2]; iter++) {
     console.log('C is fun');
   }
 }
