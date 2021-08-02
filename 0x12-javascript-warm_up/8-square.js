9499688cdfa9c06402d816976f4279bfafb31a88
DiegoSusviela <dieguitosus@hotmail.com>
2021-08-02 20:02:28 -0300

    change var name to iter, insde for condition

--- 0x12-javascript-warm_up/8-square.js
+++ 0x12-javascript-warm_up/8-square.js
@@ -2,7 +2,7 @@
 if (isNaN(process.argv[2])) {
   console.log('Missing size');
 } else {
-  for (let iter = 0; i < process.argv[2]; iter++) {
+  for (let iter = 0; iter < process.argv[2]; iter++) {
     console.log('X'.repeat(process.argv[2]));
   }
 }
