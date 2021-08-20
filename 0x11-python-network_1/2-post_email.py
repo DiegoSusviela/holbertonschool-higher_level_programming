6f0981ce32f08babdcbcba6a0177b588f79f31c5
DiegoSusviela <dieguitosus@hotmail.com>
2021-08-20 16:08:30 -0300

    changed import to be single

--- 0x11-python-network_1/2-post_email.py
+++ 0x11-python-network_1/2-post_email.py
@@ -1,12 +1,11 @@
 #!/usr/bin/python3
 """alibaba y los 40 ladrones"""
-import urllib.parse as parse
-import urllib.request as request
+import urllib
 from sys import argv
 
 if __name__ == "__main__":
 	values = {'email': argv[2]}
-    data = parse.urlencode(values).encode('ascii')
-    req = request.Request(argv[1], data)
-    with request.urlopen(req) as response:
+    data =  urllib.parse.urlencode(values).encode('ascii')
+    req =  urllib.request.Request(argv[1], data)
+    with  urllib.request.urlopen(req) as response:
         print(response.read().decode('utf-8'))
