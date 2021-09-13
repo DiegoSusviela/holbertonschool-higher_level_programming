88ac9abda2f04029ef587f66b2d5e064eb30c2ba
DiegoSusviela <dieguitosus@hotmail.com>
2021-09-12 22:49:57 -0300

    added 0 and readme

---
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+"""aa wopa necesito score"""
+
+import sys
+import MySQLdb
+
+if __name__ == "__main__":
+    a = MySQLdb.connect(host="localhost", port=3306, user=sys.argv[1],
+                           passwd=sys.argv[2], db=sys.argv[3], charset="utf8")
+    b = a.cursor()
+    b.execute("SELECT * FROM states ORDER BY id ASC")
+    q = b.fetchall()
+    for row in q:
+        print(row)
+    b.close()
+    a.close()
