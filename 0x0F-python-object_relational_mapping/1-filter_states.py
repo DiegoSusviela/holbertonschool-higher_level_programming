8a3343e1a123157312448265b0c1a3651e63ad8e
DiegoSusviela <dieguitosus@hotmail.com>
2021-09-12 22:52:41 -0300

    added 1

---
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+"""This module contains a script that lists all states starting with N"""
+
+import sys
+import MySQLdb
+
+if __name__ == "__main__":
+    a = MySQLdb.connect(host="localhost", port=3306, user=sys.argv[1],
+                           passwd=sys.argv[2], db=sys.argv[3], charset="utf8")
+    b = a.cursor()
+    b.execute(
+        "SELECT * FROM states WHERE Name LIKE BINARY 'N%' ORDER BY id ASC")
+    q = b.fetchall()
+    for row in q:
+        print(row)
+    b.close()
+    a.close()
