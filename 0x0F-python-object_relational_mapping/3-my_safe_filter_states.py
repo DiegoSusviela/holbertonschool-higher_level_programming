f1f793e7b4da20b50fcf9e727413e84645182ef1
DiegoSusviela <dieguitosus@hotmail.com>
2021-09-12 23:01:49 -0300

    added 3

---
+++ 0x0F-python-object_relational_mapping/3-my_safe_filter_states.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+"""a wopas asd"""
+
+import sys
+import MySQLdb
+
+if __name__ == "__main__":
+    a = MySQLdb.connect(host="localhost", port=3306, user=sys.argv[1],
+                           passwd=sys.argv[2], db=sys.argv[3], charset="utf8")
+    b = a.cursor()
+    b.execute("SELECT * FROM states WHERE Name =  %s ORDER BY id ASC",
+                [sys.argv[4]])
+    q = b.fetchall()
+    for row in q:
+        print(row)
+    b.close()
+    a.close()
