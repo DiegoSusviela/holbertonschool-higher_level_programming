3629bed15e6b6159712a49618b5c6bdfa62041b9
DiegoSusviela <dieguitosus@hotmail.com>
2021-09-12 23:00:05 -0300

    added 2

---
+++ 0x0F-python-object_relational_mapping/2-my_filter_states.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+"""a wopas asd"""
+
+import sys
+import MySQLdb
+
+if __name__ == "__main__":
+    a = MySQLdb.connect(host="localhost", port=3306, user=sys.argv[1],
+                        passwd=sys.argv[2], db=sys.argv[3], charset="utf8")
+    b = a.cursor()
+    b.execute(
+        "SELECT * FROM states WHERE Name LIKE BINARY '{}' ORDER BY id ASC"
+        .format(sys.argv[4]))
+    q = b.fetchall()
+    for row in q:
+        print(row)
+    b.close()
+    a.close()
