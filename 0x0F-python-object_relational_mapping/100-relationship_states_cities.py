eb9b57ea0654449c3119d6d078dc1ec76a370028
DiegoSusviela <dieguitosus@hotmail.com>
2021-09-12 23:30:49 -0300

    first advanced

---
+++ 0x0F-python-object_relational_mapping/100-relationship_states_cities.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+"""a wopas asd"""
+
+import sqlalchemy
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+from relationship_state import Base, State
+from relationship_city import City
+from sys import argv
+
+
+if __name__ == "__main__":
+    eng = create_engine("mysql+mysqldb://{}:{}@localhost/{}".format(argv[1],
+                                                                    argv[2],
+                                                                    argv[3]))
+    Base.metadata.create_all(eng)
+    a = sessionmaker(bind=eng)
+    a = a()
+    b = State(name="California")
+    b.cities = [City(name="San Francisco")]
+    a.add(b)
+    a.commit()
+    a.close()
