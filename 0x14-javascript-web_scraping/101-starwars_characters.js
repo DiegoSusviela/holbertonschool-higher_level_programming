1e5733591aab5311dc59b8fe70cddccbc08a709f
DiegoSusviela <dieguitosus@hotmail.com>
2021-09-14 23:57:48 -0300

    changed all second adva, expanded for pep

--- 0x14-javascript-web_scraping/101-starwars_characters.js
+++ 0x14-javascript-web_scraping/101-starwars_characters.js
@@ -1,18 +1,22 @@
 #!/usr/bin/node
+const filmNbr = process.argv[2];
 const request = require('request');
+const url = 'https://swapi-api.hbtn.io/api/films/' + filmNbr;
 
-request('https://swapi-api.hbtn.io/api/films/' + process.argv[2], function (error, response, body) {
-  if (error)
+request(url, function (error, response, body) {
+  if (error) {
     console.error(error);
+  }
   const path = JSON.parse(body).characters;
   console.log(path);
-  let arr = Object.keys(path).map(key => path[key]);
-  console.log(arr);
+  let wholeArray = Object.keys(path).map(key => path[key]);
+  console.log(wholeArray);
 
-  for(i = 0; i < arr.length; i++) {
-    request(arr[i], function (error, response, body) {
-      if (error)
+  for(i = 0; i < wholeArray.length; i++) {
+    request(wholeArray[i], function (error, response, body) {
+      if (error) {
         console.error(error);
+      }
       const name = JSON.parse(body).name;
       console.log(name);
     });
