d21f899413bf781dde72171e5b0d8e502d57f2dd
DiegoSusviela <dieguitosus@hotmail.com>
2021-09-14 23:31:47 -0300

    added 4

--- 0x14-javascript-web_scraping/4-starwars_count.js
+++ 0x14-javascript-web_scraping/4-starwars_count.js
@@ -1,18 +1,19 @@
 #!/usr/bin/node
 const request = require('request');
-let dic = {};
-let cont = 0;
+let resdic = {};
+let count = 0;
 let pos1 = 0;
 let pos2 = 0;
 request(process.argv[2], function (
   error, response, body) {
   if (err) throw err;
-  dic = JSON.parse(body);
-  for (pos1 = 0; pos1 < dic.results.length; pos1++) {
-    for (pos2 = 0; pos2 < dic.results[pos1].characters.length; pos2++) {
-      if (dic.results[pos1].characters[pos2].includes('/18/'))
-        cont++;
+  resdic = JSON.parse(body);
+    for (pos1 = 0; pos1 < resdic.results.length; pos1++) {
+      for (pos2 = 0; pos2 < resdic.results[pos1].characters.length; pos2++) {
+        if (resdic.results[pos1].characters[pos2].includes('/18/')) {
+          count++;
+        }
+      }
     }
-    console.log(cont);
-  }
+    console.log(count);
 });
