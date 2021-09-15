b90f3fdb1a41f9fc109d40fa0292579af1e1b913
DiegoSusviela <dieguitosus@hotmail.com>
2021-09-14 23:41:37 -0300

    added 6

--- 0x14-javascript-web_scraping/5-request_store.js
+++ 0x14-javascript-web_scraping/5-request_store.js
@@ -1,6 +1,7 @@
 #!/usr/bin/node
 const req = require('request');
-req.get(process.argv[2], function (error, response) {
+
+req.get(process.argv[2], function (err, response) {
   if (err) throw err;
   require('fs').appendFile(process.argv[3], response.body, function (err) {
     if (err) throw err;
